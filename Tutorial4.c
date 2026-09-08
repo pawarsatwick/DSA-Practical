3881e570a45a9002884bd6cb1d14732e65301318
Satwick Pawar <pawarsatu107@gmail.com>
2026-09-08 23:09:40 +0530

    Add student management functions in Tutorial4.c

--- Tutorial4.c
+++ Tutorial4.c
@@ -1,1 +1,58 @@
-
+#include <stdio.h>
+struct student
+{
+    int roll;
+    char name[80];
+    float SGPA;
+};
+void create(struct student arr[], int n);
+void display(struct student arr[], int n);
+void search(struct student arr[], int n);
+void create(struct student arr[], int n)
+{
+    printf("Enter student details:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter roll no, name and SGPA: ");
+        scanf("%d %s %f", &arr[i].roll, arr[i].name, &arr[i].SGPA);
+    }
+}
+void display(struct student arr[], int n)
+{
+    printf("\nStudent Details:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Roll No: %d\n", arr[i].roll);
+        printf("Name: %s\n", arr[i].name);
+        printf("SGPA: %.2f\n", arr[i].SGPA);
+    }
+}
+void search(struct student arr[], int n)
+{
+    int roll, i;
+    printf("\nEnter roll number to search: ");
+    scanf("%d", &roll);
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i].roll == roll)
+        {
+            printf("\nStudent found!\n");
+            printf("Roll No: %d\n", arr[i].roll);
+            printf("Name: %s\n", arr[i].name);
+            printf("SGPA: %.2f\n", arr[i].SGPA);
+            return;
+        }
+    }
+    printf("\nStudent not found.\n");
+}
+int main()
+{
+    struct student s[100];
+    int n;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+    create(s, n);
+    display(s, n);
+    search(s, n);
+    return 0;
+}
