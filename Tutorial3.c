329b4b5a481a87e62505d74e30539ccdb1beb7b6
Satwick Pawar <pawarsatu107@gmail.com>
2026-09-08 22:40:14 +0530

    Update Tutorial3.c

--- Tutorial3.c
+++ Tutorial3.c
@@ -45,21 +45,17 @@ int main()
             printf("Invalid choice!\n");
             return 0;
     }
-
     printf("\nSorted array: ");
 
     for (i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
-
     return 0;
 }
-/* ---------------- BUBBLE SORT ---------------- */
 void BubbleSort(int arr[], int n)
 {
     int i, j, temp;
-
     for (i = 0; i < n - 1; i++)
     {
         for (j = 0; j < n - i - 1; j++)
@@ -73,32 +69,27 @@ void BubbleSort(int arr[], int n)
         }
     }
 }
-/* ---------------- INSERTION SORT ---------------- */
 void InsertionSort(int arr[], int n)
 {
     int i, j, key;
-
     for (i = 1; i < n; i++)
     {
         key = arr[i];
         j = i - 1;
-
         while (j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j];
             j--;
         }
-
         arr[j + 1] = key;
     }
 }
-/* ---------------- PARTITION / QUICK SORT ---------------- */
 int Partition(int arr[], int low, int high)
 {
     int pivot;
     int i, j;
     int temp;
-
+    
     pivot = arr[high];
     i = low - 1;
 
@@ -113,50 +104,39 @@ int Partition(int arr[], int low, int high)
             arr[j] = temp;
         }
     }
-
     temp = arr[i + 1];
     arr[i + 1] = arr[high];
     arr[high] = temp;
-
     return i + 1;
 }
 void QuickSort(int arr[], int low, int high)
 {
     int p;
-
     if (low < high)
     {
         p = Partition(arr, low, high);
-
         QuickSort(arr, low, p - 1);
         QuickSort(arr, p + 1, high);
     }
 }
-/* ---------------- MERGE SORT ---------------- */
 void MergeSort(int arr[], int low, int high)
 {
     int mid;
-
     if (low < high)
     {
         mid = (low + high) / 2;
-
         MergeSort(arr, low, mid);
         MergeSort(arr, mid + 1, high);
-
         Merge(arr, low, mid, high);
     }
 }
-
 void Merge(int arr[], int low, int mid, int high)
 {
     int SA[10];
     int i, j, k;
-
     i = low;
     j = mid + 1;
     k = low;
-
     while (i <= mid && j <= high)
     {
         if (arr[i] < arr[j])
@@ -169,24 +149,20 @@ void Merge(int arr[], int low, int mid, int high)
             SA[k] = arr[j];
             j++;
         }
-
         k++;
     }
-
     while (i <= mid)
     {
         SA[k] = arr[i];
         i++;
         k++;
     }
-
     while (j <= high)
     {
         SA[k] = arr[j];
         j++;
         k++;
     }
-
     for (i = low; i <= high; i++)
     {
         arr[i] = SA[i];
