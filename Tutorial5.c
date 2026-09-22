61a7cb754ac5f24259760ff0bfc8c674f1a20594
Satwick Pawar <pawarsatu107@gmail.com>
2026-09-22 18:41:22 +0530

    Update Tutorial5.c

--- Tutorial5.c
+++ Tutorial5.c
@@ -1,3 +1,4 @@
+// stack using array
 #include <stdio.h>
 #define MAX 5
 int stack[MAX], top=-1;
@@ -38,3 +39,67 @@ int main()
     display();
     return 0;
 }
+
+// Queue using array
+#include <stdio.h>
+#define MAX 5
+int queue[MAX];
+int front= -1, rear= -1;
+void enqueue(int value){
+    if(rear== MAX -1){
+        printf("Queue is overflow\n");
+    }else {
+        if (front== -1)front=0;
+        queue[++rear]== value;
+        printf("%d inserted \n", value);
+    }
+}
+void dequeue()
+{
+    if(front== -1|| front>rear){
+        printf("Queue is empty!\n");
+    }
+    else 
+    {
+        printf("%d deleted\n", queue[front++]);
+    }
+}
+void display() {
+    if (front == -1 || front > rear) 
+    {
+        printf("Queue is Empty!\n");
+    }else {
+        printf("Queue: ");
+        for (int i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
+    }
+}
+int main(){
+    int choice, value;
+    while(1){
+        printf("\n 1.Enqueue\n 2.dequeue\n 3.display\n 4.Exit\n");
+        printf("enter choice:");
+        scanf("%d", &choice);
+        switch (choice){
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &value);
+                enqueue(value);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+}
+
+// 
